41963f3ed89b4e20a292fb5da8851851c6fa9f72
GeiseR98 <ipatovsergejj@yandex.ru>
2023-05-28 11:40:18 +0300

    feat: UserServiceImpl

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -1,30 +1,38 @@
 package ru.practicum.shareit.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+
     @Override
     public List<User> getAll() {
-        return null;
+        return userRepository.getAll();
     }
 
     @Override
     public User save(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public User update(Integer userId, User user) {
-        return null;
+        user.setId(userId);
+        return userRepository.update(user);
     }
 
     @Override
     public User getById(Integer id) {
-        return null;
+        return userRepository.getById(id);
     }
 
     @Override
     public void removeById(Integer id) {
-
+        userRepository.removeById(id);
     }
 }
