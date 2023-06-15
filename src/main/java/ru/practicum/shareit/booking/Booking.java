ad97a3f31d089130d499da59751902af471b81ba
GeiseR98 <ipatovsergejj@yandex.ru>
2023-06-15 14:42:42 +0300

    feat: Booking

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,28 +1,19 @@
 package ru.practicum.shareit.booking;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
 
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 public class Booking {
     private Integer id;
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @NotNull
     private LocalDateTime start;
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @NotNull
     private LocalDateTime end;
-    @NotNull
     private Item item;
-    @NotNull
     private User booker;
-    @NotNull
     private Status status;
 }
--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,32 @@
 package ru.practicum.shareit.booking.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import ru.practicum.shareit.booking.Status;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Data
+@AllArgsConstructor
 public class BookingDto {
+    private Integer id;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotNull
+    private LocalDateTime start;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotNull
+    private LocalDateTime end;
+    @NotNull
+    private Item item;
+    @NotNull
+    private User booker;
+    @NotNull
+    private Status status;
 }
