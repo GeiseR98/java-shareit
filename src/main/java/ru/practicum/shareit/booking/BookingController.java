691fd6987d5ac3c95a25c90f99d90218594e11ce
GeiseR98 <ipatovsergejj@yandex.ru>
2023-06-17 19:38:54 +0300

    feat: create BookingController

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,12 +1,14 @@
 package ru.practicum.shareit.booking;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * TODO Sprint add-bookings.
- */
 @RestController
 @RequestMapping(path = "/bookings")
+@RequiredArgsConstructor
+@Slf4j
 public class BookingController {
+
 }
