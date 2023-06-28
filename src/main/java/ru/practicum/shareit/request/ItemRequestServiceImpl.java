33faa1f3c5679e6b34300eed797409b5d1067a21
GeiseR98 <ipatovsergejj@yandex.ru>
2023-06-28 16:22:59 +0300

    feat: itemRequest каркас и маппер

---
+++ src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
@@ -0,0 +1,11 @@
+package ru.practicum.shareit.request;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemRequestServiceImpl implements ItemRequestService{
+}
--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,28 +1,25 @@
 package ru.practicum.shareit.request.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import ru.practicum.shareit.user.User;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.dto.ItemDto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
+import java.util.List;
 
-/**
- * TODO Sprint add-item-requests.
- */
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class ItemRequestDto {
     private Integer id;
     @NotBlank(message = "описание не может быть пустым")
     @Size(min = 1, max = 88, message = "максимальная длина описания — 88 символов")
     private String description;
-    @NotNull
-    private User requestor;
     @JsonFormat(pattern = "yyyy-MM-dd")
     @NotNull
     private LocalDateTime created;
+    private List<ItemDto> items;
 }
